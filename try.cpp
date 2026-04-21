7ce8f4dc59f2d89363216066f1526ef3b4616b97
zain7171 <zainulabidin7171@gmail.com>
2026-04-21 21:59:49 +0500

    Cleaned appointments.txt for date format

--- try.cpp
+++ try.cpp
@@ -4,6 +4,7 @@
 #include<string>
 using namespace std;
 void fixTime(fstream&);
+void fixDate(fstream&);
 int main ()
 {
     fstream app_file("appointments.txt", ios::in);
@@ -13,6 +14,14 @@ int main ()
         exit(1);
     }
     fixTime(app_file);
+    app_file.clear();
+    app_file.open("appointments.txt", ios::in);
+    if (app_file.fail())
+    {
+        cout<<"Failed to open file appointments.txt"<<endl;
+        exit(1);
+    }
+    fixDate(app_file);
     return 0;
 }
 void fixTime(fstream &timeFile)
@@ -76,6 +85,58 @@ void fixTime(fstream &timeFile)
     }
     tempFile.close();
     timeFile.close();
+    remove("appointments.txt");
+    rename("temp.txt", "appointments.txt");
+}
+void fixDate(fstream &dateFile)
+{
+    ofstream tempFile("temp.txt");
+    if (tempFile.fail())
+    {
+        cout<<"Failed to create file temp.txt"<<endl;
+        exit(1);
+    }
+    bool first = true;
+    while (true)
+    {
+        if (dateFile.peek() == -1)
+            break;
+        else
+        {
+            if (first)
+                first = false;
+            else
+                tempFile<<endl;
+            int x,y;
+            char hash;
+            dateFile>>x>>hash>>y>>hash;
+            tempFile<<x<<hash<<y<<hash;
+            int month;
+            char dash;
+            int date;
+            int year;
+            dateFile>>month;
+            if (month > 12)
+            {
+                year = month;
+                dateFile>>dash>>month>>dash>>date;
+                tempFile<<month<<dash<<date<<dash<<year;
+                string s;
+                getline(dateFile,s);
+                tempFile<<s;
+            }
+            else
+            {
+                dateFile>>dash>>date>>dash>>year;
+                tempFile<<month<<dash<<date<<dash<<year;
+                string s;
+                getline(dateFile,s);
+                tempFile<<s;
+            }
+        }
+    }
+    tempFile.close();
+    dateFile.close();
     remove("appointments.txt");
     rename("temp.txt", "appointments.txt");
 }
