5917f9bc265f6d7730a7d4b3e495ced8048b82a7
zain7171 <zainulabidin7171@gmail.com>
2026-04-24 21:29:55 +0500

    Add patient functions

--- main.cpp
+++ main.cpp
@@ -33,6 +33,8 @@ struct Treatment
 void count_records(ifstream&, int&);
 void fillPatients(ifstream&, Patient&);
 void fillDoctors(ifstream&, Doctor&);
+void addPatient(Patient*&, int&);
+void updatePatient(Patient*, int);
 int main ()
 {
     fstream app_file("appointments.txt", ios::in);
@@ -107,6 +109,64 @@ int main ()
     for (int i=0; i<total_doc; i++)
         fillDoctors(inFile,doctors[i]);
     inFile.close();
+    cout<<"============================================="<<endl;
+    cout<<"        HOSPITAL MANAGEMENT SYSTEM"<<endl;
+    cout<<"============================================="<<endl;
+    cout<<endl;
+    int id;
+    string pass;
+    cout<<"Enter Employee ID: ";
+    cin>>id;
+    cout<<"Enter Password: ";
+    cin>>pass;
+    while (id!=123 && pass!="pass123")
+    {
+        cout<<"Incorrect credentials, please try again."<<endl;
+        cout<<endl;
+        cout<<"Enter Employee ID: ";
+        cin>>id;
+        cout<<"Enter Password: ";
+        cin>>pass;
+    }
+    cout<<endl;
+    cout<<"Login Successful!"<<endl;
+    cout<<endl;
+    int choice;
+    do
+    {
+        cout<<"=================== Main Menu ========================="<<endl;
+        cout<<endl;
+        cout<<"1) Patient Management"<<endl;
+        cout<<"2) Doctor Management"<<endl;
+        cout<<"3) Appointments"<<endl;
+        cout<<"4) Treatments & Billing"<<endl;
+        cout<<"5) Search/Reports"<<endl;
+        cout<<endl;
+        cout<<"Enter your choice: ";
+        cin>>choice;
+        if (choice == 6)
+            break;
+        else
+        {
+            cout<<endl;
+            if (choice == 1)
+            {
+                int choice2;
+                cout<<"You have entered Patient Management, please select an option."<<endl;
+                cout<<"1) Add Patient"<<endl;
+                cout<<"2) Update Patient"<<endl;
+                cout<<"3) View Patient"<<endl;
+                cout<<"4) Delete Patient"<<endl;
+                cout<<endl;
+                cout<<"Enter choice: ";
+                cin>>choice2;
+                if (choice2 == 1)
+                    addPatient(patients,total_pat);
+                else if (choice2 == 2)
+                    updatePatient(patients,total_pat);
+            }
+        }
+    } while (true);
     return 0;
 }
 void count_records(ifstream &patientFile, int &total)
@@ -147,4 +207,96 @@ void fillDoctors(ifstream &inFile, Doctor &d)
     d.experience = experience;
     d.name = name;
     d.speciality = speciality;
+}
+void addPatient(Patient* &patients, int &count)
+{
+    Patient* temp = new Patient[count+1];
+    for (int i=0; i<count; i++)
+        temp[i] = patients[i];
+    delete [] patients;
+    patients = temp;
+    cout<<"Enter ID: ";
+    cin>>patients[count].patientId;
+    cout<<"Enter age: ";
+    cin>>patients[count].age;
+    cin.ignore();
+    cout<<"Enter name: ";
+    getline(cin,patients[count].name);
+    cout<<"Enter gender: ";
+    getline(cin,patients[count].gender);
+    cout<<"Enter contact: ";
+    getline(cin,patients[count].contact);
+    cout<<"Enter balance: ";
+    cin>>patients[count].balance;
+    count++;
+}
+void updatePatient(Patient* patients, int count)
+{
+    int check_id;
+    cout<<"Enter ID to update: ";
+    cin>>check_id;
+    int c;
+    cout<<"Select what field you want to update:"<<endl;
+    cout<<"1) Patient name"<<endl;
+    cout<<"2) Age"<<endl;
+    cout<<"3) Gender"<<endl;
+    cout<<"4) Contact"<<endl;
+    cout<<"5) Balance"<<endl;
+    cout<<"Enter choice: ";
+    cin>>c;
+    for (int i=0; i<count; i++)
+    {
+        if (patients[i].patientId == check_id)
+        {
+            if (c == 1)
+            {
+                cin.ignore();
+                cout<<"Enter updated name: ";
+                getline(cin,patients[i].name);
+            }
+            else if (c == 2)
+            {
+                cout<<"Enter updated age: ";
+                cin>>patients[i].age;
+            }
+            else if (c == 3)
+            {
+                cout<<"Enter updated Gender: ";
+                cin>>patients[i].gender;
+            }
+            else if (c == 4)
+            {
+                cout<<"Enter updated contact: ";
+                cin>>patients[i].contact;
+            }
+            else if (c == 5)
+            {
+                cout<<"Enter updated balance: ";
+                cin>>patients[i].balance;
+            }
+            break;
+        }
+    }
+    ofstream tempFile("temp.txt");
+    if (tempFile.fail())
+    {
+        cout<<"Failed to create file temp.txt";
+        exit(1);
+    }
+    char hash = '#';
+    bool first = true;
+    for (int i=0; i<count; i++)
+    {
+        if (first)
+            first = false;
+        else
+            tempFile<<endl;
+        tempFile<<patients[i].patientId<<hash
+        <<patients[i].name<<hash<<patients[i].age
+        <<hash<<patients[i].gender<<hash<<patients[i].contact
+        <<hash<<patients[i].balance;
+    }
+    tempFile.close();
+    remove("patients.txt");
+    rename("temp.txt", "patients.txt");
 }
