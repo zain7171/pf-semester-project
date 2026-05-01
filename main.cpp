8c3afe6432c5f2fd4e2346eafbe584e52e3f953d
zain7171 <zainulabidin7171@gmail.com>
2026-05-01 17:48:20 +0500

    Fixed update payment status

--- main.cpp
+++ main.cpp
@@ -988,15 +988,14 @@ void updatePayment(Treatment *treatments, int count)
     cin.ignore();
     bool found = false;
     bool found2 = false;
+    string d;
+    cout<<"Enter treatment: ";
+    getline(cin,d);
     for (int i=0; i<count; i++)
     {
         if (treatments[i].patientId == check_id)
         {
-            string d;
             found = true;
-            cout<<"Patient ID found."<<endl;
-            cout<<"Enter treatment: ";
-            getline(cin,d);
             if (treatments[i].description == d)
             {
                 found2 = true;
@@ -1026,81 +1025,22 @@ void updatePayment(Treatment *treatments, int count)
         exit(1);
     }
     bool first = true;
+    char hash = '#';
     for (int i=0; i<count; i++)
     {
         if (first)
             first = false;
         else
             tempFile<<endl;
-        char hash = '#';
-        tempFile<<treatments[i].patientId<<hash<<treatments[i].description
-        <<hash<<treatments[i].cost<<hash<<treatments[i].paid;
+
+        tempFile<<treatments[i].patientId<<hash
+        <<treatments[i].description<<hash
+        <<treatments[i].cost<<hash
+        <<treatments[i].paid;
     }
     tempFile.close();
     remove("treatments.txt");
     rename("temp.txt","treatments.txt");
-    ifstream records("bills.txt");
-    if (records.fail())
-    {
-        cout<<"Failed to read file bills.txt"<<endl;
-        exit(1);
-    }
-    int id,cost;
-    char hash;
-    string status;
-    int total = 0;
-    while(records>>id)
-    {
-        records>>hash>>cost>>hash>>status;
-        total++;
-    }
-    records.clear();
-    records.seekg(0);
-    int* arr = new int[total];
-    int index = 0;
-    while(records>>id)
-    {
-        records>>hash>>cost>>hash>>status;
-        arr[index] = id;
-        index++;
-    }
-    records.clear();
-    records.seekg(0);
-    ofstream temp("temp.txt");
-    if (temp.fail())
-    {
-        cout<<"Failed to create file temp.txt";
-        exit(1);
-    }
-    bool first2= true;
-    for (int i=0; i<total; i++)
-    {
-        bool paid = true;
-        int total_cost = 0;
-        for (int j=0; j<count; j++)
-        {
-            if(arr[i] == treatments[j].patientId)
-            {
-                total_cost += treatments[j].cost;
-                if(treatments[j].paid == false)
-                    paid = false;
-            }
-        }
-        if (first2)
-            first2 = false;
-        else
-            temp<<endl;
-        records>>id>>hash>>cost>>hash>>status;
-        if (paid)
-            temp<<id<<hash<<total_cost<<hash<<"Paid";
-        else
-            temp<<id<<hash<<total_cost<<hash<<"Unpaid";
-    }
-    delete [] arr;
-    records.close();
-    temp.close();
-    remove("bills.txt");
-    rename("temp.txt", "bills.txt");
 }
 void generateBill()
 {
